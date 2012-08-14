37b7a88c779390bcb1cfed69bd0f6901e006c650
Paulo Mouat <paulo.mouat@gmail.com>
2012-08-14 17:14:48 -0400

    Bare automation scripts

---
+++ gitrepos.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:paulomouat/blog
+git clone git@github.com:paulomouat/contempworks
+git clone git@github.com:paulomouat/cv
+git clone git@github.com:paulomouat/professional-homepage
+git clone git@github.com:paulomouat/paulomouat.github.com
+git clone git@github.com:paulomouat/spikes
