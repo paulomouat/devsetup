bbc97cc72a8aa852a374c65de07d2b3ae320e893
paulomouat <paulo.mouat@gmail.com>
2021-12-01 10:21:51 -0500

    Delete gitrepos.sh

--- gitrepos.sh
+++
@@ -1,6 +0,0 @@
-git clone git@github.com:paulomouat/blog
-git clone git@github.com:paulomouat/contempworks
-git clone git@github.com:paulomouat/cv
-git clone git@github.com:paulomouat/professional-homepage
-git clone git@github.com:paulomouat/paulomouat.github.com
-git clone git@github.com:paulomouat/spikes
